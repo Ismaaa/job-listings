734f5c7f3f5ed592666613a0c338dc2567873ae4
Isma <didouhismael@gmail.com>
2020-07-26 22:48:54 +0200

    Hero with responsive ikmage

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './Hero.scss';
 
-const Hero = () => (
-  <div className="Hero">
-    <img src="/assets/bg-header-mobile.svg" alt="" />
-  </div>
-);
+const Hero = () => <div className="Hero" />;
 
 export default Hero;
