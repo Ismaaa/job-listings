5063f9512cf47ead15aa00611d41987e81c22452
Isma <didouhismael@gmail.com>
2020-07-26 22:28:02 +0200

    scss support + hero component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import JobListings from './components/JobListings';
+import Hero from './components/Hero';
+import './App.scss';
 
 function App() {
-  return <JobListings />;
+  return (
+    <div className="App">
+      <Hero />
+      <JobListings />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Hero.scss';
+
+const Hero = () => (
+  <div className="Hero">
+    <img src="/assets/bg-header-mobile.svg" alt="" />
+  </div>
+);
+
+export default Hero;
